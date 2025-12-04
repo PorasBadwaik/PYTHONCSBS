59cf26b3402fddc1b187b567e25422042d58d460
PorasBadwaik <porasbadwaik737@gmail.com>
2025-12-05 01:27:32 +0530

    Add files via upload

---
+++ dictionarycreation.py
@@ -0,0 +1,5 @@
+student={"1":"poras",
+         "2":"abhay",
+         "3":"ronnie",
+         "4":"chaitanya"}
+print(student["1"])
---
+++ list.py
@@ -0,0 +1,3 @@
+name=["poras","abhay","ronnie","chaitanya"]    #list creation
+print(name)
+print(len(name))                               #lenth of list
