b82fd098ff961c90f122ab9c972e082f5f3a13a9
PorasBadwaik <porasbadwaik737@gmail.com>
2025-12-05 18:28:55 +0530

    Add files via upload

--- functionprintprimeinrange.py
+++ functionprintprimeinrange.py
@@ -1,15 +1,11 @@
-def prime_in_range(a):
-    primes = []
-    for num in range(2, a + 1):
-        is_prime = True
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                is_prime = False
-                break
-        if is_prime:
-            primes.append(num)
-    return primes
-n = int(input("Enter a number: "))
-
-result = prime_in_range(n)
-print(f"Prime numbers in the range 1 to {n} are: {result}")
+def prime(start, end):
+    for num in range(start, end + 1):
+        if num > 1:
+            for i in range(2, num):
+                if num % i == 0:
+                    break
+            else:
+                print(num)
+startrange = int(input("Enter the start of the range: "))
+endrange = int(input("Enter the end of the range: "))
+prime(startrange, endrange)
