413fdc57c28019c6c345246e68726453810dd9cf
PorasBadwaik <porasbadwaik737@gmail.com>
2025-12-19 00:31:54 +0530

    Add files via upload

--- list.py
+++ list.py
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
+
+name=["poras","abhay","ronnie","chaitanya"]    #list creation
+print(name)
+print(len(name))                               #lenth of list
+for i in name:    
+    print(i)                            #for loop
 name=["poras","abhay","ronnie","chaitanya"]    #list creation
 print(name)
 print(len(name))                               #lenth of list
 for i in name:                             #for loop
-=======
-name=["poras","abhay","ronnie","chaitanya"]    #list creation
-print(name)
-print(len(name))                               #lenth of list
-for i in name:                             #for loop
->>>>>>> b15f203c3be1641aac48da3d44620d1a8b9f3cbc
     print(i)                    #print each element in list
